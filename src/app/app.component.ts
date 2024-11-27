ec2a34d65985fb7ce796a88976ba57b74a096c20
bertapasamontes <bertapasamontes@gmail.com>
2024-11-27 20:24:47 +0100

    ex.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BudgetsListComponent } from "./components/budgets-list/budgets-list.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [BudgetsListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/components/budgets-list/budgets-list.component.ts
+++ src/app/components/budgets-list/budgets-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-budgets-list',
@@ -6,6 +6,26 @@ import { Component } from '@angular/core';
   templateUrl: './budgets-list.component.html',
   styleUrl: './budgets-list.component.scss'
 })
-export class BudgetsListComponent {
+export class BudgetsListComponent{
+  [x: string]: any;
+   
+  sumaPresu:number = 0;
+
+  
+  totalPresu = 0;
+
+  elegirServicio(value:number, event: Event):void{
+    const checkbox2 = event.target as HTMLInputElement;
+    if(checkbox2.checked){
+      this.sumaPresu += value;
+      console.log("suma presupuesto: ", this.sumaPresu);
+      this.totalPresu = this.sumaPresu;
+    } else{
+      this.sumaPresu -=value;
+      console.log("suma presupuesto: ", this.sumaPresu);
+      this.totalPresu = this.sumaPresu;
+    }
+  }
+  
 
 }
